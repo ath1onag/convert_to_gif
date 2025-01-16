f07ee63c4879e6e3af3eaf9fdc8913f0023b650e
ath1onag <161309443+ath1onag@users.noreply.github.com>
2025-01-16 13:10:57 +0300

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,75 +1,19 @@
-import aiomysql
-from config_data.config import MYSQL_HOST, MYSQL_DATABASE, MYSQL_USER, MYSQL_PASSWORD
-import logging
+import os
+import sqlite3
 
-pool = None
-
-
-async def get_db_connection():
-    """Создает асинхронное подключение к базе данных MySQL"""
-    connection = await aiomysql.connect(
-        host=MYSQL_HOST,
-        user=MYSQL_USER,
-        password=MYSQL_PASSWORD,
-        db=MYSQL_DATABASE
-    )
-    return connection
-
-async def create_table():
-    """Создаем таблицу пользователей в базе данных"""
-    connection = await get_db_connection()
-    async with connection.cursor() as cursor:
-        await cursor.execute('''CREATE TABLE IF NOT EXISTS users (
-                                id INT AUTO_INCREMENT PRIMARY KEY,
-                                user_id BIGINT NOT NULL,
-                                username VARCHAR(255),
-                                joined TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-                            )''')
-    pass
-    connection.close()
-
-async def insert_user(user_id, username):
-    """Добавляем нового пользователя в базу данных"""
-    connection = await get_db_connection()
-    async with connection.cursor() as cursor:
-        await cursor.execute('INSERT INTO users (user_id, username) VALUES (%s, %s)', (user_id, username))
-        await connection.commit()
-    connection.close()
+def create_database():# Настройка базы данных SQLite для хранения пользователей
+    if not os.path.exists("./database/database.db"):
+        conn = sqlite3.connect("./database/database.db")
+        cursor = conn.cursor()
+        # Создаём таблицу, если её ещё нет
+        cursor.execute("CREATE TABLE IF NOT EXISTS users (id INT PRIMARY KEY, username VARCHAR(100));")
+        conn.commit()
+        conn.close()
 
 
-async def get_user_settings(user_id):
-    """Получение настроек пользователя"""
-    try:
-        async with pool.acquire() as conn:
-            async with conn.cursor() as cur:
-                await cur.execute(
-                    """SELECT gif_length
-                    FROM user_settings
-                    WHERE user_id = %s;""",
-                    (user_id,)
-                )
-                result = await cur.fetchone()
-                if result:
-                    return result[0]
-                return None
-    except Exception as e:
-        logging.error(f"Ошибка при получении настроек для пользователя {user_id}: {e}")
-        return None
-
-async def save_user_settings(user_id, gif_length):
-    """Сохранение настроек пользователя"""
-    try:
-        async with pool.acquire() as conn:
-            async with conn.cursor() as cur:
-                await cur.execute(
-                    """
-                    INSERT INTO user_settings (user_id, gif_length)
-                    VALUES (%s, %s)
-                    ON DUPLICATE KEY UPDATE gif_length = %s;
-                    """,
-                    (user_id, gif_length, gif_length)
-                )
-                await conn.commit()
-                logging.info(f"Настройки для пользователя {user_id} обновлены. Длина GIF: {gif_length} секунд.")
-    except Exception as e:
-        logging.error(f"Ошибка при сохранении настроек для пользователя {user_id}: {e}")
+def insert_database(user_id, username):
+    conn = sqlite3.connect("./database/database.db")
+    cursor = conn.cursor()
+    cursor.execute(f"INSERT OR IGNORE INTO users (id, username) VALUES (?, ?)",(user_id, username))
+    conn.commit()
+    conn.close()
